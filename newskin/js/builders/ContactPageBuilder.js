6dbf37e5954b43323b24910b2bbbc3d470701dd5
Scott Mitting <scottm@swwest.com>
2024-07-29 11:02:45 -0700

    cleaned up postback

--- newskin/js/builders/ContactPageBuilder.js
+++ newskin/js/builders/ContactPageBuilder.js
@@ -8,18 +8,14 @@ export class ContactPageBuilder {
 
     /**
      * Fills the contact page when the user is logged in.
-     * @param loginStatus {LoginStatus}
-     * @param $contactForm {HTMLFormElement}
+     * @param loginStatus {LoginStatus} the user's current login status
+     * @param $contactForm {HTMLFormElement} the form on the old site for the contact us page
      */
     build(loginStatus, $contactForm) {
 
-        // TODO: submit needs to be tied to the old form
-
         // tie form to the postback on the old page
-        //$contactForm.action = 'javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$MainContent$BtnSendMessage&quot;, &quot;&quot;, true, &quot;VlgContactUs&quot;, &quot;&quot;, false, false))';
         let $submit = $contactForm.querySelector('button')
         if ($submit) {
-            //$submit.setAttribute('onclick', 'WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions("ctl00$MainContent$BtnSendMessage", "", true, "VlgContactUs", "", false, false))');
             $submit.addEventListener('click', (e) => {
                 e.preventDefault();
                 e.stopPropagation();
@@ -29,7 +25,6 @@ export class ContactPageBuilder {
         else {
             console.error('could not find submit button');
         }
-
 
         // don't fill out form if not logged in
         if (!loginStatus.loggedIn) {
@@ -58,32 +53,33 @@ export class ContactPageBuilder {
      */
     doSubmitMessagePostback($contactForm) {
 
+        // notify user the message was sent
+        const onPostbackComplete = function(html, $iframe) {
+            $iframe.remove();
+            alert('Message sent!')
+        }
+
+        // need to add data to HTML before it is written to hidden iframe
+        const onPreWriteHtml = function(html) {
+            // inject name, company, email, phone
+            html = HtmlHelper.injectTagValue(html, 'MainContent_TxtFullName', $contactForm.elements['name'].value );
+            html = HtmlHelper.injectTagValue(html, 'MainContent_TxtCompany', $contactForm.elements['company'].value );
+            html = HtmlHelper.injectTagValue(html, 'MainContent_TxtEmail', $contactForm.elements['email'].value );
+            html = HtmlHelper.injectTagValue(html, 'MainContent_TxtPhoneNumber', $contactForm.elements['phone'].value );
+
+            // inject message
+            html = HtmlHelper.injectTagInnerHtml(html, 'MainContent_TxtMessage', $contactForm.elements['message'].value );
+
+            // complete the postback
+            console.log('sending postback', html);
+            return html;
+        }
+
         // send the postback
-        AspNetPostback.runInBackground('ContactUs.aspx?reskin=no', {elementQuery: '#MainContent_BtnSendMessage'},
-            (html, $iframe) => {
-
-                // notify user the message was sent when complete
-                $iframe.remove();
-                alert('Message sent!')
-
-            }, (html) => {
-
-                // need to add data to HTML before it is written to hidden iframe
-
-                // inject name, company, email, phone
-                html = HtmlHelper.injectTagValue(html, 'MainContent_TxtFullName', $contactForm.elements['name'].value );
-                html = HtmlHelper.injectTagValue(html, 'MainContent_TxtCompany', $contactForm.elements['company'].value );
-                html = HtmlHelper.injectTagValue(html, 'MainContent_TxtEmail', $contactForm.elements['email'].value );
-                html = HtmlHelper.injectTagValue(html, 'MainContent_TxtPhoneNumber', $contactForm.elements['phone'].value );
-
-                // inject message
-                html = HtmlHelper.injectTagInnerHtml(html, 'MainContent_TxtMessage', $contactForm.elements['message'].value );
-
-                // complete the postback
-                console.log('sending postback', html);
-                return html;
-            });
-
+        AspNetPostback.runInBackground(
+            'ContactUs.aspx?reskin=no',
+            {elementQuery: '#MainContent_BtnSendMessage'},
+            onPostbackComplete,
+            onPreWriteHtml);
     }
-
 }
